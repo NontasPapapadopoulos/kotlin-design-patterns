48d36a459ad26d09f817c4a0e4cda32271d526bf
nondas <papadopoulose92@gmail.com>
2025-02-09 20:07:31 +0200

    Decorator pattern example with Kotlin.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+package org.example
+
+import java.time.Clock
+import java.util.*
+
+
+fun interface Logger {
+    fun log(message: String)
+}
+
+val consoleLogger = Logger { println(it)  }
+
+
+fun Logger.withUniqueId() = Logger { log("${UUID.randomUUID()} $it") }
+fun Logger.withThreadName() = Logger { log("$it (on ${Thread.currentThread().name} thread)") }
+fun Logger.withDateTime(calendar: Calendar = Calendar.getInstance()) = Logger { log("[${calendar.time}] $it") }
+
+fun main() {
+
+    val logger = consoleLogger
+        .withDateTime()
+        .withThreadName()
+        .withUniqueId()
+
+    logger.log("Kotlin rocks!!")
+
+}
